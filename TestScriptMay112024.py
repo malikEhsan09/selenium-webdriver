ce2162501e05491ccf2e8a60014fd21aa261e8c9
mimrancomsats <66168186+mimrancomsats@users.noreply.github.com>
2024-05-11 12:52:39 +0500

    Create TestScriptMay112024.py

---
+++ TestScriptMay112024.py
@@ -0,0 +1,30 @@
+# vim test.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+title = driver.title
+assert title == "Web form"
+
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+text_box.send_keys("Selenium")
+submit_button.click()
+
+message = driver.find_element(by=By.ID, value="message")
+value = message.text
+print(value)
+
+assert value == "Received!"
+driver.quit()
